a2acd4da232692e0b6a8f9869347fae6756157d8
ChandeDeVargas <chande07devargas@gmail.com>
2026-03-18 17:18:43 -0400

    Add faker factory

---
+++ test_data/faker_factory.py
@@ -0,0 +1,11 @@
+"""
+Faker Test Data Factory
+Genera data de prueba realista usando Faker
+
+Útil para tests que necesitan:
+- Nombres de usuarios
+- Emails
+- Direcciones
+- Teléfonos
+- Textos random
+"""
